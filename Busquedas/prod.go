8dc4361ef0b79b6199626480656fa2ff23617aeb
jhygt46 <misitiodelivery@gmail.com>
2022-06-14 22:18:24 -0400

    aa

--- Busquedas/prod.go
+++ Busquedas/prod.go
@@ -205,8 +205,6 @@ func (h *MyHandler) HandleFastHTTP(ctx *fasthttp.RequestCtx) {
 					Res := Respuesta{Prods: make([]ResProd, 0, P.L), Emps: make([]ResEmp, 0, P.L), Count: 0}
 					cat := ParamBytes(ctx.QueryArgs().Peek("c"))
 
-					fmt.Println("CAT_CUAD", cat, p.C[0])
-
 					for _, cuad := range p.C {
 						key = append(cat, Int32tobytes(cuad)...)
 						val, _ := h.Db.Get(key)
