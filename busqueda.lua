753d505adbedf7ca887e486b51f23708590fb86f
jhygt46 <misitiodelivery@gmail.com>
2022-06-15 23:37:07 -0400

    aa

--- busqueda.lua
+++ busqueda.lua
@@ -1,5 +1,5 @@
 request = function()
     param_value = math.random(1,49)
-    path = '/?c='..param_value..'&p={"O":[1,1,1],"D":0,"C":[1,2,3,4],"F":[[1],[2],[3],[2,4,5]],"E":[1,2,3,4]}'
+    path = '/?c='..param_value..'&p={"O":[1,1,1],"D":0,"C":[1,2,3,4]}'
     return wrk.format("GET", path)
 end
