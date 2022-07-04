77e29ea1440ac7a8920beaea2f29bc7f9140929f
jhygt46 <misitiodelivery@gmail.com>
2022-07-04 17:41:21 -0400

    aa

--- AutoComplete1/test2.lua
+++
@@ -1,7 +0,0 @@
-request = function()
-    param_value1 = math.random(140910,141010)
-    param_value2 = math.random(140910,141010)
-    param_value3 = math.random(140910,141010)
-    path = '/auto?c=['..param_value1..','..param_value2..','..param_value3..']&l=1'
-    return wrk.format("GET", path)
-end
--- AutoComplete2/test1.lua
+++
@@ -1,6 +0,0 @@
-request = function()
-    param_value1 = math.random(140910,141010)
-    param_value2 = math.random(140910,141010)
-    path = '/auto?c=['..param_value1..','..param_value2..']'
-    return wrk.format("GET", path)
-end
