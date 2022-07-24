efb3ea48d12a51ecf1ee5a61f434279db10f53ee
jhygt46 <misitiodelivery@gmail.com>
2022-07-24 01:59:46 -0400

    aa

--- SitioWeb3/prod.go
+++ SitioWeb3/prod.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/mithorium/secure-fasthttp"
 	"github.com/valyala/fasthttp"
 )
 
@@ -13,20 +12,21 @@ func requestHandler(ctx *fasthttp.RequestCtx) {
 }
 
 func main() {
-	secureMiddleware := secure.New(secure.Options{
-		SSLRedirect: true,
-		SSLHost:     "localhost:443", // This is optional in production. The default behavior is to just redirect the request to the HTTPS protocol. Example: http://github.com/some_page would be redirected to https://github.com/some_page.
-	})
+	/*
+		secureMiddleware := secure.New(secure.Options{
+			SSLRedirect: true,
+			SSLHost:     "localhost:443", // This is optional in production. The default behavior is to just redirect the request to the HTTPS protocol. Example: http://github.com/some_page would be redirected to https://github.com/some_page.
+		})
 
-	secureHandler := secureMiddleware.Handler(requestHandler)
+		secureHandler := secureMiddleware.Handler(requestHandler)
 
-	// HTTP
-	go func() {
-		log.Fatal(fasthttp.ListenAndServe(":80", secureHandler))
-	}()
-
+		// HTTP
+		go func() {
+			log.Fatal(fasthttp.ListenAndServe(":80", secureHandler))
+		}()
+	*/
 	// HTTPS
 	// To generate a development cert and key, run the following from your *nix terminal:
 	// go run $GOROOT/src/pkg/crypto/tls/generate_cert.go --host="localhost"
-	log.Fatal(fasthttp.ListenAndServeTLS(":443", "/etc/letsencrypt/live/www.draescorza.cl/fullchain.pem", "/etc/letsencrypt/live/www.draescorza.cl/privkey.pem", secureHandler))
+	log.Fatal(fasthttp.ListenAndServeTLS(":443", "/etc/letsencrypt/live/www.draescorza.cl/fullchain.pem", "/etc/letsencrypt/live/www.draescorza.cl/privkey.pem", requestHandler))
 }
